e756d29e04fe93b5554bec24f616256c3d21b4f6
toastedfrog <toastedfrog@yahoo.com>
2016-07-10 16:56:41 -0700

    Begun changing logic for add error

--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -7,9 +7,13 @@ class UdaciList
   end
   def add(type, description, options={})
     type = type.downcase
-    @items.push TodoItem.new(description, options) if type == "todo"
-    @items.push EventItem.new(description, options) if type == "event"
-    @items.push LinkItem.new(description, options) if type == "link"
+    if type == "todo"
+    	@items.push TodoItem.new(description, options)
+    elsif type == "event"
+    	@items.push EventItem.new(description, options)
+    elsif type == "link"
+    	@items.push LinkItem.new(description, options)
+    end
   end
   def delete(index)
     @items.delete_at(index - 1)
