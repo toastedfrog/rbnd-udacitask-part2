73fab8c57ec13051b3b0659cbb2182ad7e1244e0
toastedfrog <toastedfrog@yahoo.com>
2016-07-10 18:40:45 -0700

    Colorized arrows

--- lib/listable.rb
+++ lib/listable.rb
@@ -16,9 +16,9 @@ module Listable
   end
   
   def format_priority(priority)
-    value = " ⇧" if priority == "high"
-    value = " ⇨" if priority == "medium"
-    value = " ⇩" if priority == "low"
+    value = " ⇧".colorize(:red) if priority == "high"
+    value = " ⇨".colorize(:yellow) if priority == "medium"
+    value = " ⇩".colorize(:green) if priority == "low"
     value = "" if !priority
     return value
   end
